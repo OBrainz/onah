1caf7545627daf906befcd5f89422bdab1bdb561
OBrainz <Lordopko@gmail.com>
2024-07-31 13:59:13 +0100

    Update onah.sh

--- onah.sh
+++ onah.sh
@@ -60,7 +60,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("$ONAH","$ONH"){} 
+    constructor()ERC20("$TOKEN_NAME","$TOKEN_SYMBOL"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
